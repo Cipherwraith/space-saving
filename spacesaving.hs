737718afdad8ead320c91a58bd288ca4c9b05029
Art <cipherwraith666@gmail.com>
2013-12-13 08:51:50 +0800

    Code refactoring
    http://www.reddit.com/r/haskell/comments/1sj5su/spacesaving_algorithm_in_haskell_approximate_the/ce08jbf

--- spacesaving.hs
+++ spacesaving.hs
@@ -3,43 +3,45 @@ import qualified Data.Map.Lazy as M
 import Data.Char (toLower)
 import Data.List (minimumBy, maximumBy)
 
+
 spaceSaving :: (Ord a) 
             => Int              
             -> [a]              
             -> M.Map a Integer  
 
+
 spaceSaving k = spaceSave M.empty 
   where
     spaceSave m [] = m
     spaceSave m (s:ss) 
-      | M.member s m = update $ M.adjust (+ 1) s m
-      | M.size m < k = update $ M.insert s 1 m
+      | M.member s m = update $ M.adjust (+ 1) s m 
+      | M.size m < k = update $ M.insert s 1 m 
       | otherwise    = update $ updateLowestKey m s
      where
       update f = spaceSave f ss
 
 
 updateLowestKey :: (Ord k) 
-                 => M.Map k Integer -- Input map
-                 -> k               -- Input value
-                 -> M.Map k Integer -- Updated map 
+                 => M.Map k Integer 
+                 -> k               
+                 -> M.Map k Integer 
+
 
 updateLowestKey m s = M.insert s (lowestValue + 1) lowestKeyRemoved
   where
-    (lowestKey, lowestValue) = head . M.toList . minimum' $ m
+    (lowestKey, lowestValue) = minimum' m
     lowestKeyRemoved = M.delete lowestKey m
    
-minimum' :: (Ord a) => M.Map k a -> M.Map k a
-minimum' = toSingleton . minimumBy comp . M.toList
-  where
-    comp (_, x) (_, y) = compare x y
-    toSingleton (k,a) = M.singleton k a
 
-maximum' :: (Ord a) => M.Map k a -> M.Map k a
-maximum' = toSingleton . maximumBy comp . M.toList
-  where
-    comp (_, x) (_, y) = compare x y
-    toSingleton (k,a) = M.singleton k a
+
+minimum' :: (Ord a) => M.Map k a -> (k, a)
+minimum' = minimumBy comp . M.toList
+
+maximum' :: (Ord a) => M.Map k a -> (k, a)
+maximum' = maximumBy comp . M.toList
+
+comp (_, x) (_, y) = compare x y
+
 
 main :: IO ()
 main = do
